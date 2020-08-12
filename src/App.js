f19f6cc020ed171cc153a6cccbf68202c321d687
EmberLynn <ember.robb@gmail.com>
2020-08-12 10:11:41 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import data from './data/data.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  render() {
+
+  let jsondata = data.map((data) => {
+    return <p>{data.content}</p>
+  })
+
+    return (
+      <div className="App">
+        <h1>Hello World</h1>
+        {jsondata}
+      </div>
+    );
+  }
 }
 
 export default App;
