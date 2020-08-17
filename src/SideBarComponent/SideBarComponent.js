d306108d3e6e3e42d8afce258898290d38172529
EmberLynn <ember.robb@gmail.com>
2020-08-17 15:04:42 -0600

    Sidebar does not display if screen is to small

--- src/SideBarComponent/SideBarComponent.js
+++ src/SideBarComponent/SideBarComponent.js
@@ -1,21 +1,10 @@
 import React from 'react'
+import './SideBarComponent.css'
 
 const sideBarComponent = () => {
-    const style = {
-        backgroundColor: '#cccccc',
-        color: 'black',
-        font: 'inherit',
-        border: '1px solid #d9d9d9',
-        boxShadow: '5px 5px 4px #999999',
-        padding: '8px',
-        width: '20%',
-        margin: '16px auto',
-        position: 'absolute',
-        left: '30px'
-      }
       
       return(
-          <div style={style}>
+          <div className="SideBarComponent">
               <p>Sidebar content goes here</p>
           </div>
       )
